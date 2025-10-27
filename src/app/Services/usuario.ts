bb73eaccb172bf724b6b4ebc734b1a077c922ff2
Matias Brizuela <63812706+MatiBri@users.noreply.github.com>
2025-10-27 17:15:49 -0300

    Update usuario.ts

--- src/app/Services/usuario.ts
+++ src/app/Services/usuario.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 
 //Solicitudes HTTP
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs'; //Esto nos permite poder recibir las respuestas de las APIs
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environments'
-import { ResponseApi } from '../Interfaces/response-api'; //Importamos la interfaz que permite recibir la respuesta de las solicitudes HTTP el cual creamos como response-api
+import { ResponseApi } from '../Interfaces/response-api';
 import { Login } from '../Interfaces/login';
 import { Usuario } from '../Interfaces/usuario';
 
@@ -12,21 +12,16 @@ import { Usuario } from '../Interfaces/usuario';
   providedIn: 'root'
 })
 export class UsuarioService {
-  //Creo las variables para armar las URL de la APi
-  private urlApi:string = environment.endpoint + "Usuario/"; //http://localhost:5042/api/Usuarios/
+  private urlApi:string = environment.endpoint + "Usuario/";
 
-  //Inyectamos el HttpClient en el constructor
   constructor(private http:HttpClient){}
 
-  //Recibe un request del tipo Login
   iniciarSesion(request:Login):Observable<ResponseApi>{
-    //Ejecuta el método iniciarSesion de la API. La URL sería /api/USuario/IniciarSesion en Swagger
     return this.http.post<ResponseApi>(`${this.urlApi}IniciarSesion`, request)
   }
 
   lista():Observable<ResponseApi>{
-    //api/Producto/Lista en Swagger
-    return this.http.get<ResponseApi>(`${this.urlApi}Lista`) //Esto tiene que coincidir con el nombre del método en el controlador de la API en Swagger
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
   }
 
   guardar(request:Usuario):Observable<ResponseApi>{
@@ -38,8 +33,6 @@ export class UsuarioService {
   }
 
   eliminar(id: number):Observable<ResponseApi>{
-    //Esto es la construcción de la URL para eliminar un usuario
-    //http://localhost:5042/api/Usuario/Eliminar/3 -> Ejemplo. Revisar Swagger
     return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`)
   }
 }
