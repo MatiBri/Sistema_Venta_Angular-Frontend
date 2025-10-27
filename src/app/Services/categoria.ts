f1dd4fc7c931682317230d64818e6189a47d102f
Matias Brizuela <63812706+MatiBri@users.noreply.github.com>
2025-10-27 17:17:08 -0300

    Update categoria.ts

--- src/app/Services/categoria.ts
+++ src/app/Services/categoria.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 //Solicitudes HTTP
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs'; //Esto nos permite poder recibir las respuestas de las APIs
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environments'
 import { ResponseApi } from '../Interfaces/response-api'; 
 
@@ -10,12 +10,10 @@ import { ResponseApi } from '../Interfaces/response-api';
   providedIn: 'root'
 })
 export class CategoriaService {
-  //URL
   private urlApi:string = environment.endpoint + "Categoria/";
   
   constructor(private http:HttpClient){}
 
-  //Lista de Categorias
   lista():Observable<ResponseApi>{
     return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
   }
