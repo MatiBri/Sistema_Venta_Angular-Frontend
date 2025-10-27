54a8f01a199c07a7a93455d818f88304eb81224b
Matias Brizuela <63812706+MatiBri@users.noreply.github.com>
2025-10-27 17:43:06 -0300

    Update dash-board.component.ts

--- src/app/Components/layout/Pages/dash-board/dash-board.component.ts
+++ src/app/Components/layout/Pages/dash-board/dash-board.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-//Importaciones
 import { Chart, registerables } from 'chart.js';
 
-// Material & CDK
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
@@ -14,18 +12,15 @@ import { MatIconModule, MatIcon } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 
-// Utiles
 import Swal from 'sweetalert2';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from '@angular/material/input';
 
-//Servicios
 import { DashBoardService } from '../../../../Services/dash-board';
 
-//Graficos
-Chart.register(...registerables); //Para trabajar con los graficos de chat.js
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dash-board',
@@ -41,19 +36,16 @@ Chart.register(...registerables); //Para trabajar con los graficos de chat.js
 })
 export class DashBoardComponent implements OnInit {
 
-  //Variables
   totalIngresos: string= "0";
   totalVentas: string = "0";
   totalProductos: string = "0";
 
   constructor(
-    //Inyeccion de dependencia
     private _dashboardServicio: DashBoardService
   ){
 
   }
 
-  //Método para mostrar la información en un gráfico
   mostrarGrafico(labelGrafico: any[], dataGrafico: any[]){
     const charBarras = new Chart('chartBarras',{
       type:"bar",
@@ -81,7 +73,7 @@ export class DashBoardComponent implements OnInit {
           }
         }
       }
-    }); //Nombre del gráfico
+    });
   }
 
   ngOnInit(): void {
@@ -92,7 +84,6 @@ export class DashBoardComponent implements OnInit {
           this.totalVentas = data.value.totalVentas;
           this.totalProductos = data.value.totalProductos;
 
-          //Informacion para pintar en el gráfico
           const arrayData : any[] = data.value.ventasUltimaSemana;
 
           const labelTemp = arrayData.map((value) => value.fecha);
